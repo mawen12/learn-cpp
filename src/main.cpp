f7e55e2d60d431e67659cc011e4ba21167287bd7
mawen12 <1181963012mw@gmail.com>
2025-01-21 20:43:44 +0800

    add main example

---
+++ src/main.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+  /*
+  // 在控制台输出 cout -> console out
+  //            endl -> ending line -> \n
+  cout << "Hello world!" << endl;
+  cout << "Mawen" << endl;
+
+  cout << "    /|" << endl;
+  cout << "   /_|" << endl;
+  cout << "  /__|" << endl;
+  cout << " /___|" << endl;
+  cout << "/____|" << endl;
+  */
+
+  /*
+  string characterName = "John";
+  int characterAge;
+  characterAge = 35;
+  // 使用 << 来拼接内容
+  cout << "There once was a man named " << characterName << endl;
+  cout << "He was "<< characterAge <<" years old" << endl;
+
+  characterName = "Mawen";
+  cout << "He liked the name " << characterName << endl;
+  cout << "But did not like being " << characterAge << endl;
+  */
+
+  /*
+  // 字符类型，仅存储单个字符
+  char grade = 'A';
+  // 字符串，存储多个字符
+  string phrase = "Mawen";
+  // 整数
+  int age = 32;
+  // 浮点数
+  double gpa = 2.4;
+  // 布尔
+  bool isMale = false;
+  cout << grade << endl;
+  */
+
+  /*
+  // 实现字符的拼接
+  string phrase = "Giraffe Academy";
+  cout << phrase;
+  cout << " World!" << endl;
+  cout << phrase.length() << endl;
+  cout << phrase[0];
+
+  phrase[0] = 'M';
+  cout << phrase << endl;
+  // 返回匹配该字符的开始索引位置
+  cout << phrase.find("Academy", 0) << endl;
+  cout << phrase.find("ffe", 0) << endl;
+  // 返回从开始索引起到指定数量之后的字符串内容
+  cout << phrase.substr(8, 3) << endl;
+  string phrasesub;
+  phrasesub = phrase.substr(8, 3);
+  cout << phrasesub << endl;
+  */
+
+
+
+  return 0;
+}
